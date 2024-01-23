5f2e1688babdd949d8d73e19edd8ab8e0fd4c58d
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-01-24 00:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (35.41%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int rob(std::vector<int>& nums) {
-    int n = nums.size();
-    if (n == 0) {
-        return 0;
-    } else if (n == 1) {
-        return nums[0];
+    int dp[405];
+    int rob(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        int n = nums.size();
+        return func(0, n-1, nums);
     }
-    vector<int> dp(n, 0);
-    dp[0]=nums[0];
-    dp[1] = max(nums[0], nums[1]);
-    for(int i= 2; i < n; i++){
-        dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+
+    int func(int idx, int end, vector<int>& nums){
+        if(idx > end) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        int currSkipped = func(idx+1, end, nums);
+        int currAdded = func(idx+2, end, nums) + nums[idx];
+        return dp[idx] = max(currSkipped, currAdded);
     }
-    return dp[n-1];
-}
 };
