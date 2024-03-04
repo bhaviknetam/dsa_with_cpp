9645d1958c1ce51b5d68a0cc0489e3cc71252741
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-03-04 19:37:40 +0530

    Time: 5 ms (28.62%) | Memory: 11 MB (51.96%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        multiset<int> s;
-        while (n--) {
-            nums1.pop_back();
+        int r1 = m -1, r2 = n-1;
+        for(int i = n +m-1;i>=0;i--){
+            if(r1 >=0 and r2 >=0)
+            nums1[i] = (nums1[r1] > nums2[r2]) ? nums1[r1--] : nums2[r2--];
+            else if(r1 >= 0){
+                nums1[i] = nums1[r1--];
+            }
+            else if(r2 >= 0){
+                nums1[i] = nums2[r2--];
+            }
+            
         }
-        for (auto i : nums1) {
-
-            s.insert(i);
-        }
-        for (auto i : nums2) {
-            s.insert(i);
-        }
-        // nums1.clear();
-        nums1 = {s.begin(), s.end()};
     }
 };
