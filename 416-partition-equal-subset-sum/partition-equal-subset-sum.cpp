71eb437470395df226fa1c17a8a6c7fccb02a1f3
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-03-21 20:20:20 +0530

    Time: 3 ms (99.91%) | Memory: 13.2 MB (85.26%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -6,8 +6,6 @@ public:
             sum+=i;
         }
         if(sum & 1) return false;
-        int msb = __builtin_clz(sum);
-        msb=1<<(31-msb);
         bitset<20000> b;
         b[0]=1;
         for(int& i : nums){
