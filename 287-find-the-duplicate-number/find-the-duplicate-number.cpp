e3fe14a371d237a30880346392cd94f6908207bb
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-04-04 17:21:13 +0530

    Time: 74 ms (76.45%) | Memory: 63.5 MB (93.26%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -4,7 +4,7 @@ public:
         for(int i = 0; i < nums.size(); i++){
             int x = abs(nums[i]) - 1;
             if(nums[x] < 0) return abs(nums[i]);
-            nums[x] = (-1)*nums[x];
+            nums[x] = -nums[x];
         }
         return -1;
     }
