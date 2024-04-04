c387c74c69c295a2c213682a4202f24251f7fafb
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-04-04 16:53:45 +0530

    Time: 3 ms (40.00%) | Memory: 7.4 MB (78.96%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -4,11 +4,11 @@ public:
         int ans = 0;
         int count = 0;
         for (char& i : s) {
-            if (i == '(')
+            if (i == '(') {
                 count++;
-            else if (i == ')')
+                ans = max(ans, count);
+            } else if (i == ')')
                 count--;
-            ans = max(ans, count);
         }
         return ans;
     }
