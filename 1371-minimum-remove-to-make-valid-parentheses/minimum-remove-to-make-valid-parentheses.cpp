968213b33f01fd3c6dbca2f0130dd92fc7041705
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-04-07 01:03:50 +0530

    Time: 25 ms (31.31%) | Memory: 12.9 MB (56.98%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -2,30 +2,25 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         string ans = "";
-        unordered_set<int> store;
         stack<int> st;
         for (int i = 0; i < s.size(); i++) {
-
-            if (s[i] == ')' and st.empty())
+            if (s[i] == ')' and st.empty()) {
+                s[i] = '@';
                 continue;
+            }
             if (s[i] == '(') {
+                s[i] = '@';
                 st.push(i);
                 continue;
             }
             if (s[i] == ')' and !st.empty()) {
                 int temp = st.top();
-                store.insert(i);
-                store.insert(temp);
+                s[temp] = '(';
                 st.pop();
             }
         }
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(' and !store.count(i)) {
-                continue;
-            }
-            if (s[i] == ')' and !store.count(i)) {
-                continue;
-            } else
+            if (s[i] != '@')
                 ans.push_back(s[i]);
         }
         return ans;
