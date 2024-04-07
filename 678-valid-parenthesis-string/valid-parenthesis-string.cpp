6e912e2deab7b1ac377b6be70873943cf8cfdaa9
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-04-07 16:29:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (39.07%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> left, star;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                left.push(i);
+            }else if(s[i]=='*'){
+                star.push(i);
+            }else{
+                if(!left.empty()){
+                    left.pop();
+                }else if(!star.empty()){
+                    star.pop();
+                }else return false;
+            }
+        }
+        if(left.size()>star.size()) return false;
+        while(!left.empty() and !star.empty()){
+            if(left.top() > star.top()){
+                return false;
+            }
+            left.pop();
+            star.pop();
+        }
+        return true;
+    }
+};
