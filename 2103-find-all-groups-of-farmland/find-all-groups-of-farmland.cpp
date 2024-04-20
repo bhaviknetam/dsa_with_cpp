2b0452c32eee720060d3d2b4fe2a2335e0ce4456
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-04-20 11:28:19 +0530

    Time: 92 ms (88.36%) | Memory: 70.6 MB (66.18%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
+
+    void dfs(vector<vector<int>>&land, int i, int j , int &gx, int &gy){
+        int n=land.size(),m=land[0].size();
+        if(i>=0 && j>=0 && i<n && j<m && land[i][j]==1){
+            land[i][j]=0;
+            gx=max(gx,i),gy=max(gy,j);
+            dfs(land,i+1,j,gx,gy);
+            dfs(land,i-1,j,gx,gy);
+            dfs(land,i,j+1,gx,gy);
+            dfs(land,i,j-1,gx,gy);
+        }
+    }
+    
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        int n = land.size();
-        int m = land[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        vector<vector<int>> ans;
-        stack<pair<int, int>> stk;
-
-        auto isValid = [&](int i, int j) {
-            return i >= 0 && j >= 0 && i < n && j < m && !vis[i][j] && land[i][j] == 1;
-        };
-
-        auto dfs = [&](int i, int j) {
-            int maxx = i, maxy = j;
-            stk.push({i, j});
-            vis[i][j] = true;
-
-            while (!stk.empty()) {
-                auto [x, y] = stk.top();
-                stk.pop();
-                maxx = max(maxx, x);
-                maxy = max(maxy, y);
-
-                vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-                for (auto [dx, dy] : dirs) {
-                    int nx = x + dx, ny = y + dy;
-                    if (isValid(nx, ny)) {
-                        stk.push({nx, ny});
-                        vis[nx][ny] = true;
-                    }
-                }
-            }
-
-            ans.push_back({i, j, maxx, maxy});
-        };
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (isValid(i, j)) {
-                    dfs(i, j);
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int n= land.size();
+        int m= land[0].size();
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(land[i][j]==1){
+                    int lx=i,ly=j,gx=i,gy=j;
+                    dfs(land,i,j,gx,gy);
+                    ans.push_back({lx,ly,gx,gy});
                 }
             }
         }
-
         return ans;
     }
 };
