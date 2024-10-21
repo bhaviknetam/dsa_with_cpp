739684a355ecbbdc1a4ab457db250ea9bca1fe8c
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-10-21 12:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 94.6 MB (93.50%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,11 +3,26 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
-        for(int val : nums2){
-            nums1.push_back(val);
-        }
-        sort(nums1.begin(), nums1.end());
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
         int n = n1 + n2;
-        return (n&1)?nums1[n/2]:(nums1[n/2]+nums1[n/2 - 1])/2.0;
+        int left = (n + 1) / 2;
+        int lo = 0, hi = n1;
+        while (lo <= hi) {
+            int mid1 = (lo + hi) >> 1;
+            int mid2 = left - mid1;
+            int l1 = (mid1 - 1 >= 0) ? nums1[mid1 - 1] : INT_MIN;
+            int r1 = (mid1 < n1) ? nums1[mid1] : INT_MAX;
+            int l2 = (mid2 - 1 >= 0) ? nums2[mid2 - 1] : INT_MIN;
+            int r2 = (mid2 < n2) ? nums2[mid2] : INT_MAX;
+            if (l1 > r2)
+                hi = mid1 - 1;
+            else if (l2 > r1)
+                lo = mid1 + 1;
+            else
+                return (n & 1) ? max(l1, l2)
+                               : (max(l1, l2) + min(r1, r2)) / 2.0;
+        }
+        return 0;
     }
 };
