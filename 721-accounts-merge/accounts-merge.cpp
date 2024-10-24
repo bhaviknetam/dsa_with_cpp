84d976538d6711e3c1715d16a5a6404bec6d822c
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-10-24 20:26:32 +0530

    Time: 45 ms (96.30%) | Memory: 38.7 MB (46.97%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,36 +1,54 @@
 class Solution {
+private: unordered_map<string, int> emailPar;
+        vector<int> par;
 public:
+    void init(int n){
+        for(int i = 0; i < n; i++){
+            par[i] = i;
+        }
+    }
+    
+    int find(int x){
+        while(x!=par[x]){
+            par[x] = par[par[x]];
+            x = par[x];
+        }
+        return x;
+    }
+
+    void Union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a != b) par[a] = par[b];
+    }
+    
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        unordered_map<string, unordered_set<string>> adj;
-        unordered_set<string> vis;
-        for (auto& acc : accounts) {
-            string first = acc[1];
-            for (int i = 2; i < acc.size(); i++) {
-                adj[first].insert(acc[i]);
-                adj[acc[i]].insert(first);
-            }
-        }
-        function<void(string, vector<string>&)> dfs =
-            [&](string node, vector<string>& store) {
-                vis.insert(node);
-                store.push_back(node);
-                for (auto& neighbor : adj[node]) {
-                    if (vis.find(neighbor) == vis.end()) {
-                        dfs(neighbor, store);
-                    }
+        int n = accounts.size();
+        par.resize(n, 0);
+        init(n);
+        for(int i = 0; i < n;  i++){
+            for(int j = 1; j < accounts[i].size(); j++){
+                if(emailPar.find(accounts[i][j]) == emailPar.end())
+                    emailPar[accounts[i][j]] = i;
+                else{
+                    Union(i, emailPar[accounts[i][j]]);
                 }
-            };
-        vector<vector<string>> mergedAccounts;
-        for (auto& acc : accounts) {
-            string name = acc[0];
-            if (vis.find(acc[1]) == vis.end()) {
-                vector<string> store;
-                store.push_back(name);
-                dfs(acc[1], store);
-                sort(store.begin() + 1, store.end());
-                mergedAccounts.push_back(store);
             }
         }
-        return mergedAccounts;
+        vector<set<string>> v(n);
+        for(auto it : emailPar){
+            v[find(it.second)].insert(it.first);
+        }
+        vector<vector<string>> mergedAccs;
+        for(int i = 0; i <n; i++){
+            if(v[i].empty())continue;
+            vector<string> mergedAcc;
+            mergedAcc.push_back(accounts[i][0]);
+            for(auto& str : v[i]){
+                mergedAcc.push_back(str);
+            }
+            mergedAccs.push_back(mergedAcc);
+        }
+        return mergedAccs;
     }
 };
