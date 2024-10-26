824901503440b80ce2d7fe1f72c5613d623b6ab9
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-10-26 18:29:25 +0530

    Time: 71 ms (98.21%) | Memory: 117.7 MB (57.95%) - LeetSync

--- 1788-stone-game-vi/stone-game-vi.cpp
+++ 1788-stone-game-vi/stone-game-vi.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
         int n = aliceValues.size();
-        priority_queue<pair<int, int>> pq;
+        vector<pair<int, int>> pq;
         for(int i = 0; i < n; i++){
-            pq.push({aliceValues[i]+bobValues[i], i});
+            pq.push_back({aliceValues[i]+bobValues[i], i});
         }
+        sort(pq.begin(), pq.end(), greater<pair<int, int>>());
         bool isAlice = true;
-        int alice = 0, bob = 0;
-        while(!pq.empty()){
+        int alice = 0, bob = 0, i = 0;
+        while(i < n){
             if(isAlice){
-                alice += aliceValues[pq.top().second];
+                alice += aliceValues[pq[i].second];
             }else{
-                bob += bobValues[pq.top().second];
+                bob += bobValues[pq[i].second];
             }
-            pq.pop();
             isAlice = !isAlice;
+            i++;
         }
         if(alice > bob) return 1;
         else if(alice < bob) return -1;
