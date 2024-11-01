a3e52641c6f0aadd0d3a73d16ceeb68af485a095
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-11-01 19:39:00 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (40.11%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -9,25 +9,47 @@
  * };
  */
 class Solution {
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        auto comp = [](ListNode* l1, ListNode* l2){
-            return l1->val > l2->val;
-        };
-        priority_queue<ListNode*,vector<ListNode*>,decltype(comp)> pq(comp);
-        for(auto& list : lists){
-            if(list) pq.push(list);
-        }
+private:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode* head = new ListNode();
         ListNode* curr = head;
-        while(!pq.empty()){
-            auto list = pq.top();
-            pq.pop();
-            curr->next = list;
-            list = list->next;
-            if(list) pq.push(list);
+        while (l1 and l2) {
+            if (l1->val < l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
             curr = curr->next;
         }
-        return head->next;
+        while (l1) {
+            curr->next = l1;
+            l1 = l1->next;
+            curr = curr->next;
+        }
+        while (l2) {
+            curr->next = l2;
+            l2 = l2->next;
+            curr = curr->next;
+        }
+        curr = head->next;
+        delete head;
+        return curr;
+    }
+
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+        if(n < 1) return NULL;
+        int adder = 1;
+        while (adder < n) {
+            for (int i = 0; i + adder < n; i += (adder << 1)) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + adder]);
+            }
+            adder <<= 1;
+        }
+
+        return lists[0];
     }
 };
