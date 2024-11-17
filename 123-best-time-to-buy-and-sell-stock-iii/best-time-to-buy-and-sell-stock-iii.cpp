4ce4bc13843e9b8329aaf216b4c9e014536bfd75
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-11-18 00:17:40 +0530

    Time: 429 ms (25.45%) | Memory: 220.9 MB (33.54%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,27 @@
 class Solution {
-private: vector<vector<vector<int>>> dp;
+private:
+    vector<vector<vector<int>>> dp;
+
 public:
-    int func(int idx, vector<int>& prices, bool bought,int k){
+    int func(int idx, vector<int>& prices, bool bought, int k) {
+        if (k == 0 || idx == prices.size()) return 0;
+        if (dp[idx][bought][k] != -1) return dp[idx][bought][k];
         int ans = 0;
-        if(k == 0 || idx == prices.size()) return 0;
-        if(dp[idx][bought][k] != -1) return dp[idx][bought][k];
-        if(bought){
-            ans = max(ans, prices[idx] + func(idx + 1,prices, false,k - 1));
-            ans = max(ans,func(idx + 1, prices, true, k));
-        }else{
-            ans=max(ans,-prices[idx]+func(idx + 1, prices, true, k));
-            ans=max(ans,func(idx + 1, prices, false, k));
+
+        if (bought) {
+            ans = max(ans, prices[idx] + func(idx + 1, prices, 0 ,k - 1));
+            ans = max(ans, func(idx + 1, prices, 1, k));
+        } else {
+            ans = max(ans, -prices[idx] + func(idx + 1, prices, 1, k));
+            ans = max(ans, func(idx + 1, prices, 0, k));
         }
+        
         return dp[idx][bought][k] = ans;
     }
+    
     int maxProfit(vector<int>& prices) {
-        dp.resize(prices.size(), vector<vector<int>>(2, vector<int>(3, -1)));
+        int n = prices.size();
+        dp.resize(n, vector<vector<int>>(2, vector<int>(3, -1)));
         return func(0, prices, false, 2);
     }
 };
