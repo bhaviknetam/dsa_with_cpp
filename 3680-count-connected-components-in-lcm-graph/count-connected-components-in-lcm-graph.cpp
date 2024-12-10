0330fbab265cf4952b9680c095813156f9dca175
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-12-10 11:53:07 +0530

    Time: 939 ms (29.85%) | Memory: 104.5 MB (45.50%) - LeetSync

--- 3680-count-connected-components-in-lcm-graph/count-connected-components-in-lcm-graph.cpp
+++ 3680-count-connected-components-in-lcm-graph/count-connected-components-in-lcm-graph.cpp
@@ -5,20 +5,21 @@ private:
 
 public:
     int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]); // Path compression
+        while(x != parent[x]){
+            parent[x] = parent[parent[x]];
+            x = parent[x];
         }
-        return parent[x];
+        return x;
     }
 
     void Union(int a, int b) {
         a = find(a);
         b = find(b);
         if (a != b) {
-            if(s.count(a) && s.count(b))
+            if(s.find(a) != s.end() && s.find(b) != s.end())
                 if (a > b)
                     swap(a, b);
-            else if(!s.count(a)){
+            else if(s.find(a) != s.end()){
                 swap(a, b);
             }
             parent[b] = a;
@@ -33,14 +34,14 @@ public:
         for(int i = mini; i <= threshold; i++){
             for(int j = 1; j * j <= i; j++){
                 if(i % j == 0){
-                    if(s.count(j))
+                    if(s.find(j) != s.end())
                         Union(j, i);
-                    if(i/j != j && s.count(i/j))
+                    if(i/j != j && s.find(i/j) != s.end())
                         Union(i/j, i);
                 }
             }
         }
-        set<int> ans;
+        unordered_set<int> ans;
         for(int &num:nums){
             if(num <= threshold)
                 ans.insert(find(num));
