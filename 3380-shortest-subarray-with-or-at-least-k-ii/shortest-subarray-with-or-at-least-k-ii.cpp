1fd6e456b17bc4d4f8a9a97969210185b22bec5a
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2024-12-23 20:49:48 +0530

    Time: 292 ms (12.51%) | Memory: 260.1 MB (5.00%) - LeetSync

---
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> countArr(n + 1, vector<int>(32, 0));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 32; j++) {
+                countArr[i][j] += countArr[i - 1][j];
+                if ((nums[i - 1] >> j) & 1)
+                    countArr[i][j]++;
+            }
+        }
+        int totalOR = 0, ans = INT_MAX;
+        int j = 1;
+        for (int i = 1; i <= n; i++) {
+            totalOR |= nums[i - 1];
+            while (j <= i && totalOR >= target) {
+                ans = min(ans, i - j + 1);
+                int nextOR = 0;
+                for (int k = 0; k < 32; k++) {
+                    int ct = countArr[i][k] - countArr[j][k];
+                    if (ct > 0)
+                        nextOR |= (1 << k);
+                }
+                totalOR = nextOR;
+                j++;
+            }
+        }
+        return (ans != INT_MAX) ? ans : -1;
+    }
+};
