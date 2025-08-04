ab8cd0e46314ef4d4d078c3c1d98ee32d01fe013
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-04 12:10:10 +0530

    Time: 3 ms (89.64%) | Memory: 23.8 MB (80.10%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -7,12 +7,12 @@ public:
         vector<vector<int>> ans;
         while(i < n){
             int j = i + 1;
-            int maxval = intervals[i][1];
-            while(j < n and intervals[j][0] <= maxval){
-                maxval = max(maxval,  intervals[j][1]);
+            int end = intervals[i][1];
+            while(j < n && end >= intervals[j][0]){
+                end = max(end, intervals[j][1]);
                 j++;
             }
-            ans.push_back({intervals[i][0], maxval});
+            ans.push_back({intervals[i][0], end});
             i = j;
         }
         return ans;
