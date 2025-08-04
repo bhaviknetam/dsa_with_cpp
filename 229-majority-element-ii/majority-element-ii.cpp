f17b6ce11e76187abe9382eb57764a571de27021
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-05 00:20:10 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (94.06%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -3,23 +3,14 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int ct1 = 0, ct2 = 0, cd1 = 0, cd2 = 1;
         for (int& num : nums) {
-            if (num == cd1) {
-                ct1++;
-            } else if (num == cd2) {
-                ct2++;
-            } else if (ct1 == 0) {
-                cd1 = num;
-                ct1 = 1;
-            } else if (ct2 == 0) {
-                cd2 = num;
-                ct2 = 1;
-            } else {
-                ct1--;
-                ct2--;
-            }
+            if(num == cd1) ct1++;
+            else if(num == cd2) ct2++;
+            else if(ct1 == 0) cd1 = num, ct1 = 1;
+            else if(ct2 == 0) cd2 = num, ct2 = 1;
+            else ct1--, ct2--;
         }
         // cout << cd2 <<';';
-        ct1 = 0, ct2 = 0;
+        ct1 = ct2 = 0;
         for (int num : nums) {
             if (num == cd1) {
                 ct1++;
@@ -33,7 +24,7 @@ public:
             ans.push_back(cd1);
         if (ct2 > n / 3)
             ans.push_back(cd2);
-        cout<< cd1<<';'<<cd2;
+        // cout<< cd1<<';'<<cd2;
         return ans;
     }
 };
