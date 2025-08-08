c7769284efec3f9a3c2132ae89c8a0ccfe975de3
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-08 19:11:36 +0530

    Time: 12 ms (38.99%) | Memory: 11.8 MB (53.20%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,14 +9,10 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(!head || !head->next) return false;
         ListNode* hare = head;
         ListNode* tortoise = head;
-        while(true){
-            if(!hare) return false;
-            hare = hare->next;
-            if(!hare) return false;
-            hare = hare->next;
+        while(hare && hare->next){
+            hare = hare->next->next;
             tortoise = tortoise->next;
             if(hare == tortoise) return true;
         }
