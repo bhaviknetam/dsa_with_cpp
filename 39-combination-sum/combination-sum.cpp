6e9d68a76f0a77fb45edd17017d424172095704c
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-11 19:29:32 +0530

    Time: 26 ms (20.34%) | Memory: 20.9 MB (20.77%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -7,15 +7,12 @@ public:
             ans.push_back(curr);
         }
         for(int j = i; j < candidates.size(); j++){
-            int c = candidates[j];
-            if(target < c) continue;
-            curr.push_back(c);
-            backtrack(j, target - c, curr, candidates);
+            curr.push_back(candidates[j]);
+            backtrack(j, target - candidates[j], curr, candidates);
             curr.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
         vector<int> v;
         backtrack(0, target, v, candidates);
         return ans;
