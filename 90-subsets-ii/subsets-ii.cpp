e849a40faee221820555a51920bc5caf8cac4a6d
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-11 17:16:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (19.31%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,9 +1,23 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+
+struct VectorHash {
+    size_t operator()(const vector<int>& v) const {
+        size_t h = v.size();
+        for (int x : v) {
+            h ^= (x + 0x9e3779b97f4a7c15ULL + (h << 6) + (h >> 2));
+        }
+        return h;
+    }
+};
+
 class Solution {
-private: vector<vector<int>> s;
+private: gp_hash_table<vector<int>, null_type, VectorHash> s;
 public:
     void helper(int i, vector<int>& curr, vector<int>& nums){
         if(i == nums.size()){
-            s.push_back(curr);
+            s.insert(curr);
             return;
         }
         curr.push_back(nums[i]);
@@ -16,6 +30,6 @@ public:
         vector<int> curr;
         sort(nums.begin(), nums.end());
         helper(0, curr, nums);
-        return s;
+        return vector<vector<int>>(s.begin(), s.end());
     }
 };
