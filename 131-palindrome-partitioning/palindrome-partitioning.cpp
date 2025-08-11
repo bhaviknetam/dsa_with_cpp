d9d9afc47fc2c38db40ef8dcc15ee12e313c36af
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-11 23:20:16 +0530

    Time: 74 ms (21.54%) | Memory: 84.1 MB (20.29%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -17,17 +17,12 @@ public:
     }
     vector<vector<string>> partition(string s) {
         vector<string> p;
-
         int n = s.size();
         vector<vector<bool>> dp(n, vector<bool>(n, false));
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = i; j < n; j++){
-                if(j == i){
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (s[i] == s[j] && (j - i <= 2 || dp[i + 1][j - 1])) {
                     dp[i][j] = true;
-                }else if(j - i == 1){
-                    dp[i][j] = (s[i] == s[j]);
-                }else{
-                    dp[i][j] = (s[i] == s[j]) & dp[i+1][j-1];
                 }
             }
         }
