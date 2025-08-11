ed50bb226cff8240eb8445f70616d847f40aa9a3
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-11 22:49:37 +0530

    Time: 108 ms (17.72%) | Memory: 128.1 MB (16.68%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,36 +1,41 @@
 class Solution {
 private: vector<vector<string>> ans;
 public:
-    bool isPalindrome(string& s){
-        int i = 0, j = s.size()-1;
-        while(i < j){
-            if(s[i] != s[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-
-    void helper(int i, vector<string>& p, string s){
-        if(i == s.size()){
-            if(isPalindrome(p.back()))
+    void helper(int i, vector<string>& p, string s, vector<vector<bool>>& dp){
+        int n = s.size();
+        if(i == n){
+            int sz = p.back().size();
+            if(dp[n - sz][n - 1])
                 ans.push_back(p);
             return;
         }
-        if(p.empty() || isPalindrome(p.back())){
+        if(p.empty() || dp[i - p.back().size()][i - 1]){
             p.push_back(string() + s[i]);
-            helper(i+1, p, s);
+            helper(i+1, p, s, dp);
             p.pop_back();
         }
         if(i != 0){
             p.back().push_back(s[i]);
-            helper(i+1, p, s);
+            helper(i+1, p, s, dp);
             p.back().pop_back();
         }
     }
     vector<vector<string>> partition(string s) {
         vector<string> p;
-        helper(0, p, s);
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = i; j < n; j++){
+                if(j == i){
+                    dp[i][j] = true;
+                }else if(j - i == 1){
+                    dp[i][j] = (s[i] == s[j]);
+                }else{
+                    dp[i][j] = (s[i] == s[j]) & dp[i+1][j-1];
+                }
+            }
+        }
+        helper(0, p, s, dp);
         return ans;
     }
 };
