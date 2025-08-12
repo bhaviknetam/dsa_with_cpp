6a7e89dbedcb2d9ee8336cd17185a8aee59fb285
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-12 17:43:40 +0530

    Time: 6 ms (24.80%) | Memory: 11.1 MB (31.75%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    vector<vector<string>> ans;
+    vector<bool> c, ad;
+    unordered_set<int> d;
+public:
+    void helper(int row, int n, vector<string>& s){
+        if(row == n){
+            ans.push_back(s);
+            return;
+        }
+        for(int col = 0; col < n; col++){
+            if(!c[col] && !d.count(row - col) && !ad[row + col]){
+                c[col]  = ad[row + col] = true;
+                d.insert(row - col);
+                s[row][col] = 'Q';
+                helper(row + 1, n, s);
+                s[row][col] = '.';
+                d.erase(row - col);
+                c[col] = ad[row + col] = false;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        c.resize(n, false);
+        ad.resize(2 * n, false);
+        string k;
+        for(int i = 0; i < n; i++) k.push_back('.');
+        vector<string> s;
+        for(int i = 0; i < n; i++) s.push_back(k);
+        helper(0, n, s);
+        return ans;
+    }
+};
