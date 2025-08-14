17ed91771fca6bcc19bb2aa152e82203b583abcb
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-14 23:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (94.67%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,23 +4,17 @@ public:
         int n = nums.size();
         int l = -1, r = n - 1;
         while(r - l > 1){
-            int mid = l + ((r-l)>>1);
-            if(nums[mid] >= nums[l + 1] && nums[mid] >= nums[r]){
-                l = mid;
-            }else{
-                r = mid;
-            }
-        }
-        int k = n - r;
-        k %= n;
-        // cout << r <<';';
-        l = r, r = n + l;
-        while(r - l > 1){
-            int mid = l + ((r-l)>>1);
-            if(nums[mid % n] <= target) l = mid;
+            int mid = l + ((r - l) >> 1);
+            if(nums[mid] > nums[n-1]) l = mid;
             else r = mid;
         }
-        l %= n;
-        return (nums[l] == target) ? l : -1;
+        if(target <= nums[n-1]) l = r, r = n;
+        else l = 0;
+        while(r - l > 1){
+            int mid = l + ((r-l)>>1);
+            if(nums[mid] <= target) l = mid;
+            else r = mid;
+        }
+        return (nums[l] == target ? l : -1);
     }
 };
