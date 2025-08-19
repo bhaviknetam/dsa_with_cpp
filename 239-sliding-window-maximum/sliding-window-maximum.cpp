e4c82b2564c6323b7dc7aaab3ccefe70b4a06974
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-19 19:55:47 +0530

    Time: 166 ms (11.78%) | Memory: 198.3 MB (6.87%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,17 +1,82 @@
+struct Node {
+    int index; // store index of array element
+    Node* prev;
+    Node* next;
+    Node(int i) : index(i), prev(NULL), next(NULL) {}
+};
+
+// Doubly Linked List class for our custom deque
+class DLL {
+    Node* head;
+    Node* tail;
+
+public:
+    DLL() : head(NULL), tail(NULL) {}
+
+    bool empty() { return head == NULL; }
+
+    int front() { return head->index; }
+
+    int back() { return tail->index; }
+
+    void pop_front() {
+        if (head) {
+            Node* temp = head;
+            head = head->next;
+            if (head)
+                head->prev = NULL;
+            else
+                tail = NULL;
+            delete temp;
+        }
+    }
+
+    void pop_back() {
+        if (tail) {
+            Node* temp = tail;
+            tail = tail->prev;
+            if (tail)
+                tail->next = NULL;
+            else
+                head = NULL;
+            delete temp;
+        }
+    }
+
+    void push_back(int i) {
+        Node* node = new Node(i);
+        if (!head) {
+            head = tail = node;
+        } else {
+            tail->next = node;
+            node->prev = tail;
+            tail = node;
+        }
+    }
+};
+
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> d;
-        vector<int> ans;
-        for(int i = 0; i < nums.size(); i++){
-            while(!d.empty() && nums[d.back()] <= nums[i]){
-                d.pop_back();
-            }
-            d.push_back(i);
-            if(d.front() == i - k) d.pop_front();
-            if(i >= k-1)
-                ans.push_back(nums[d.front()]);
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> result;
+        DLL dq;
+
+        for (int i = 0; i < n; i++) {
+            // Remove indices out of this window
+            if (!dq.empty() && dq.front() <= i - k)
+                dq.pop_front();
+
+            // Remove smaller elements (they can't be max)
+            while (!dq.empty() && arr[dq.back()] <= arr[i])
+                dq.pop_back();
+
+            dq.push_back(i);
+
+            // Record answer when we have a full window
+            if (i >= k - 1)
+                result.push_back(arr[dq.front()]);
         }
-        return ans;
+        return result;
     }
 };
