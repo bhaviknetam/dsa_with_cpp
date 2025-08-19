0898f3c3737df435c3732f92a4d5acc82d9f3d9a
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-19 19:48:09 +0530

    Time: 16 ms (86.24%) | Memory: 139.2 MB (35.46%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,24 +1,16 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/detail/standard_policies.hpp>
-using namespace __gnu_pbds;
-
-using ordered_map = tree<int, int, greater<int>, rb_tree_tag, tree_order_statistics_node_update>;
-
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        ordered_map m;
-        for(int i = 0; i < k; i++){
-            m[nums[i]]++;
-        }
-        // cout << m.size() <<';';
+        deque<int> d;
         vector<int> ans;
-        ans.push_back(m.begin()->first);
-        for(int i = k; i < nums.size(); i++){
-            m[nums[i]]++;
-            if(m[nums[i-k]] == 1) m.erase(nums[i-k]);
-            else m[nums[i-k]]--;
-            ans.push_back(m.begin()->first);
+        for(int i = 0; i < nums.size(); i++){
+            while(!d.empty() && nums[d.back()] <= nums[i]){
+                d.pop_back();
+            }
+            d.push_back(i);
+            if(d.front() == i - k) d.pop_front();
+            if(i >= k-1)
+                ans.push_back(nums[d.front()]);
         }
         return ans;
     }
