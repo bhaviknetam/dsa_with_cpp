0862e4e353cf81faaa7683aba12d68d0fd260bad
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-22 19:27:49 +0530

    Time: 859 ms (5.16%) | Memory: 15.8 MB (39.63%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,49 +1,36 @@
 class Solution {
-private:
-    string temp;
-
 public:
-    vector<int> manacher_odd(string s) {
-        temp = s;
+    void LCP(string& s, vector<int>& lcp){
         int n = s.size();
-        s = "$" + s + "^";
-        vector<int> p(n + 2);
-        int l = 1, r = 1;
-        for (int i = 1; i <= n; i++) {
-            p[i] = max(0, min(r - i, p[l + (r - i)]));
-            // cout << p[i]
-            while (s[i - p[i]] == s[i + p[i]]) {
-                p[i]++;
-            }
-            if (i + p[i] > r) {
-                l = i - p[i], r = i + p[i];
+        int i = 1, len = 0;
+        lcp[0] = 0;
+        while(i < n){
+            if(s[i] == s[len]){
+                len++;
+                lcp[i] = len;
+                i++;
+            }else{
+                if(len != 0){
+                    len = lcp[len - 1];
+                }else{
+                    lcp[i] = 0;
+                    i++;
+                }
             }
         }
-        return vector<int>(begin(p) + 1, end(p) - 1);
-    }
-
-    vector<int> manacher(string s) {
-        string t;
-        for (auto c : s) {
-            t += string("#") + c;
-        }
-        auto res = manacher_odd(t + "#");
-        // cout << res.size() << '\n';
-        return vector<int>(begin(res), end(res));
     }
     string shortestPalindrome(string s) {
-        if(s.size() <= 1) return s;
-        auto ans = manacher(s);
-        int longest_length = 0;
-        for (int i = 0; i < ans.size(); i++) {
-            if (i - ans[i] + 1 == 0) {
-                longest_length = 2*ans[i] - 1;
-            }
-        }
-        string t = temp.substr(0, longest_length);
-        t.erase(remove(t.begin(), t.end(), '#'), t.end());
-        string a = s.substr(t.size(), s.size() - t.size());
-        reverse(a.begin(), a.end());
-        return a + s;
+        string t = s;
+        reverse(t.begin(), t.end());
+        t = s + "$" + t;
+        vector<int> lcp(t.size());
+        LCP(t, lcp);
+        int n = s.size();
+        for(int i:lcp)cout <<i<<';';
+        string ans = s;
+        reverse(ans.begin(), ans.end());
+        int ct = n - lcp.back();
+        for(int i = n - ct; i < n; i++) ans.push_back(s[i]);
+        return ans;
     }
 };
