0a8dd5243f8c5c6c3d8eae6775f9e160bbbc318a
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-26 15:44:17 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (28.65%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,32 +11,15 @@
  */
 class Solution {
 public:
+    bool func(TreeNode* a, TreeNode* b){
+        if(!a || !b){
+            return a == b;
+        }
+        if(a->val != b->val) return false;
+        return func(a->left, b->right) && func(a->right, b->left);
+    }
     bool isSymmetric(TreeNode* root) {
         if(!root) return true;
-        queue<pair<TreeNode*, TreeNode*>> q;
-        if(root->left && root->right){
-            if(root->left->val != root->right->val) return false;
-            q.push({root->left, root->right});
-        }
-        else if(root->left || root->right) 
-            return false;
-        else return true;
-        while(!q.empty()){
-            auto [node1, node2] = q.front();
-            q.pop();
-            if(node1->left && node2->right){
-                if(node1->left->val != node2->right->val) return false;
-                q.push({node1->left, node2->right});
-            }
-            else if(node1->left || node2->right) 
-                return false;
-            
-            if(node1->right && node2->left){
-                if(node1->right->val != node2->left->val) return false;
-                q.push({node1->right, node2->left});
-            }
-            else if(node1->right || node2->left) return false;
-        }
-        return true;
+        return func(root->left, root->right);
     }
 };
