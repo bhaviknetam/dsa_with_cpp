9adb764a9594591ab618a119691a00a5219a6333
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-28 14:56:53 +0530

    Time: 2 ms (57.09%) | Memory: 37.3 MB (60.55%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,56 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
-struct BSTIter {
-    std::stack<TreeNode*> st;
-    bool forward; // true => next smallest (inorder), false => next largest (reverse inorder)
-
-    BSTIter(TreeNode* root, bool forward) : forward(forward) {
-        pushPath(root);
-    }
-
-    void pushPath(TreeNode* node) {
-        while (node) {
-            st.push(node);
-            node = forward ? node->left : node->right;
-        }
-    }
-
-    // Returns next node in chosen order, or nullptr when exhausted
-    TreeNode* next() {
-        if (st.empty()) return nullptr;
-        TreeNode* node = st.top(); st.pop();
-        if (forward) pushPath(node->right);
-        else         pushPath(node->left);
-        return node;
-    }
-};
-
 class Solution {
 public:
+    void inorder(TreeNode* root, vector<int>& vt) {
+        if (!root) return;
+        inorder(root->left, vt) , vt.push_back(root->val) , inorder(root->right, vt);
+    }
+
     bool findTarget(TreeNode* root, int k) {
-        if (!root) return false;
-
-        BSTIter L(root, /*forward=*/true);   // smallest upwards
-        BSTIter R(root, /*forward=*/false);  // largest downwards
-
-        TreeNode* a = L.next();
-        TreeNode* b = R.next();
-
-        while (a && b && a != b) {
-            long long sum = (long long)a->val + (long long)b->val; // avoid overflow
+        vector<int> nums;
+        inorder(root, nums);
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int sum = nums[l] + nums[r];
             if (sum == k) return true;
-            if (sum < k)  a = L.next();
-            else          b = R.next();
+            else if (sum < k) l++;
+            else r--;
         }
         return false;
     }
