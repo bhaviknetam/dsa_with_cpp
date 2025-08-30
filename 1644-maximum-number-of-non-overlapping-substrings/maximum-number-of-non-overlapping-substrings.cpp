6ee714492749801a00dd44f1b65dff10b6d94dd1
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-08-31 01:17:34 +0530

    Time: 69 ms (36.59%) | Memory: 23.3 MB (87.48%) - LeetSync

--- 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.cpp
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     vector<string> maxNumOfSubstrings(string s) {
-        int n = s.size();
-        vector<pair<int, int>> v(26, {-1, -1});
-        for(int i = 0; i < n; i++){
-            if(v[s[i] - 'a'].first == -1){
-                v[s[i] - 'a'].first = i;
-            }
-            v[s[i] - 'a'].second = i;
-        }
-        vector<string> res;
-        int lastRight = -1;
-        for(int i = 0; i < n; i++){
-            auto p = v[s[i]-'a'];
-            if(i != p.first) continue;
-            int j = p.first, last = p.second;
-            while(j <= last){
-                auto np = v[s[j] - 'a'];
-                if(np.first < i){
-                    last = -1;
-                    break;
-                }
-                last = max(last, np.second);
-                j++;
-            }
-            if(last == -1) continue;
-            if(last > lastRight) res.push_back("");
-            lastRight = last;
-            res.back() = s.substr(i, last - i + 1);
-        }
-        return res;
+        vector<int> start(26, INT_MAX), end(26, INT_MIN), skip(26, 0);
+        vector<string> result;
+
+        for (int i = 0; i < s.size(); ++i)
+            start[s[i] - 'a'] = min(start[s[i] - 'a'], i), end[s[i] - 'a'] = i;
+
+        for (int i = 0; i < 26; ++i)
+            for (int j = start[i]; j <= end[i]; ++j)
+                if (start[s[j] - 'a'] < start[i])
+                    skip[i] = 1;
+                else
+                    end[i] = max(end[i], end[s[j] - 'a']);
+
+        for (int i = s.size() - 1, cut = INT_MAX; i >= 0; --i)
+            if (i == start[s[i] - 'a'] && end[s[i] - 'a'] < cut &&
+                !skip[s[i] - 'a'])
+                result.push_back(s.substr((cut = i), end[s[i] - 'a'] - i + 1));
+
+        return result;
     }
 };
