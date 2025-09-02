e4e9665ee033f98b358d7ee8773be898448b3163
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-09-02 14:55:58 +0530

    Time: 41 ms (14.29%) | Memory: 14 MB (25.74%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,25 +1,23 @@
 class Solution {
+private:
+    vector<vector<int>> dp;
 public:
-    struct PairHash{
-        template<class T1, class T2>
-        size_t operator()(const pair<T1, T2>& p) const{
-            auto h1 = hash<T1>{}(p.first);
-            auto h2 = hash<T2>{}(p.second);
-            return h1 ^ (h2 << 1);
-        }
-    };
-
-    unordered_map<pair<int, int>, int, PairHash> m;
     int func(int l, int r, vector<int>& cuts){
-        if(m.find({l, r}) != m.end()) return m[{l, r}];
+        if(l >= r) return 0;
+        if(dp[l][r] != -1) return dp[l][r];
         int ans = INT_MAX;
-        for(int& cut : cuts){
-            if(cut > l && cut < r)
-                ans = min(ans, r - l + func(l, cut, cuts) + func(cut, r, cuts));
+        for(int i = l + 1; i < r; i++){
+            int cut = cuts[i];
+            ans = min(ans, cuts[r] - cuts[l] + func(l, i, cuts) + func(i, r, cuts));
         }
-        return m[{l,r}] = (ans == INT_MAX ? 0 : ans);
+        return dp[l][r] = (ans == INT_MAX ? 0 : ans);
     }
     int minCost(int n, vector<int>& cuts) {
-        return func(0, n, cuts);
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+        int k = cuts.size();
+        dp.assign(k, vector<int>(k, -1));
+        return func(0, k - 1, cuts);
     }
 };
