0388d40d513e8aaa1749cefaaaf15924e313e785
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-09-03 00:29:50 +0530

    Time: 51 ms (80.16%) | Memory: 12 MB (48.28%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    vector<int> cutdp;
-    int helper(int start, string& s, vector<vector<bool>>& dp){
-        int n = s.size();
-        if(start == n) return -1;
-        if(cutdp[start] != -1) return cutdp[start];
-        int ans = INT_MAX, res;
-        for(int i = start; i < n; i++){
-            if(dp[start][i]){
-                res = 1 + helper(i + 1, s, dp);
-                if(res < ans) ans = res;
-            }
-        }
-        return cutdp[start] = ans;
-    }
     int minCut(string s) {
         int n = s.size();
         vector<vector<bool>> dp(n, vector<bool>(n, false));
@@ -29,7 +15,7 @@ public:
                 }
             }
         }
-        cutdp.assign(n + 1, 1e8);
+        vector<int> cutdp(n + 1, 1e8);
         cutdp[n] = -1;
         for(int i = n - 1; i >= 0; i--){
             for(int j = i; j < n; j++){
