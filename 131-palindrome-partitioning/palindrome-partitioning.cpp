82783d3447ebcd137198dfb6b3bb398c6d6560f2
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-09-02 23:25:07 +0530

    Time: 29 ms (92.40%) | Memory: 53.5 MB (76.39%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,35 +1,35 @@
 class Solution {
-private: vector<vector<string>> ans;
 public:
-    void helper(int i, vector<string>& p, string s, vector<vector<bool>>& dp){
+    bool check(string& s){
         int n = s.size();
-        if(i == n){
-            ans.push_back(p);
+        int l = 0, r = n - 1;
+        while(l < r){
+            if(s[l] != s[r]) return false;
+            l++, r--;
+        }
+        return true;
+    }
+    void helper(int start, string& s, vector<string>& ans,
+    vector<vector<string>>& res){
+        if(start == s.size()){
+            res.push_back(ans);
             return;
         }
-        for(int j = i; j < n; j++){
-            if(dp[i][j]){
-                p.push_back(s.substr(i, j - i + 1));
-                helper(j + 1, p, s, dp);
-                p.pop_back();
+        string t;
+        for(int i = start; i < s.size(); i++){
+            t.push_back(s[i]);
+            if(check(t)){
+                ans.push_back(t);
+                helper(i + 1, s, ans, res);
+                ans.pop_back();
             }
         }
     }
+
     vector<vector<string>> partition(string s) {
-        vector<string> p;
-        int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = i; j < n; ++j) {
-                if (s[i] == s[j] && (j - i <= 2 || dp[i + 1][j - 1])) {
-                    dp[i][j] = true;
-                }
-            }
-        }
-        helper(0, p, s, dp);
-        return ans;
+        vector<vector<string>> res;
+        vector<string> ans;
+        helper(0, s, ans, res);
+        return res;
     }
 };
-
-auto init = atexit([]()
-{ ofstream("display_runtime.txt") << "0";});
