3323b244ec3a352c00277968314c8c056d8748ee
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-09-03 00:11:18 +0530

    Time: 64 ms (72.18%) | Memory: 12.2 MB (34.58%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
+    vector<int> cutdp;
+    int helper(int start, string& s, vector<vector<bool>>& dp){
+        int n = s.size();
+        if(start == n) return -1;
+        if(cutdp[start] != -1) return cutdp[start];
+        int ans = INT_MAX, res;
+        for(int i = start; i < n; i++){
+            if(dp[start][i]){
+                res = 1 + helper(i + 1, s, dp);
+                if(res < ans) ans = res;
+            }
+        }
+        return cutdp[start] = ans;
+    }
     int minCut(string s) {
         int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n, 0));
-        for(int i =0 ; i< n;i++){
-            dp[i][i] = 1;
-        }
-        for(int i= n-2; i >= 0 ;i--){
-            for(int j = i+1; j < n; j++){
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int i=0; i <n;i++) dp[i][i] = true;
+        for(int i = n-2; i >= 0; i--){
+            for(int j = i + 1; j < n; j++){
+                if(j - i == 1){
+                    dp[i][j] = (s[i] == s[j]);
+                    continue;
+                }
                 if(s[i] == s[j]){
-                    if(j - i == 1){
-                        dp[i][j] = 1;
-                    }else{
-                        dp[i][j] = dp[i+1][j-1];
-                    }
+                    dp[i][j] = dp[i+1][j-1];
                 }
             }
         }
-        vector<int> cuts(n, INT_MAX);
-        cuts[0] = 0;
-        for(int i = 1; i < n; i++){
-            if(dp[0][i]){
-                cuts[i] = 0;
-                continue;
-            }
-            for(int k = 0; k < i; k++){
-                if(dp[k+1][i] and cuts[k] + 1 < cuts[i]){
-                    cuts[i] = cuts[k] + 1;
-                }
-            }
-        }
-        return cuts[n-1];
+        cutdp.assign(n + 1, -1);
+        return helper(0, s, dp);
     }
 };
