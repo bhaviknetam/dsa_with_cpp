0f75fa94fe1faf928de49888b5ab09aca797045e
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-09-07 12:07:26 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (60.11%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> vec;
-    void subs(auto nums, int n, int m){
-        if(n == m){
-            ans.push_back(vec);
-            return;
-        }
-        subs(nums + 1, n + 1, m);
-        vec.push_back(*nums);
-        subs(nums + 1, n+1, m);
-        vec.pop_back();
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        subs(nums.begin(), 0, nums.size());
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int N = 1 << n;
+        for(int mask = 0; mask < N; mask++){
+            vector<int> v;
+            for(int i = 0; i < n; i++){
+                if(mask & (1 << i)){
+                    v.push_back(nums[i]);
+                }
+            }
+            ans.push_back(v);
+        }
         return ans;
     }
 };
