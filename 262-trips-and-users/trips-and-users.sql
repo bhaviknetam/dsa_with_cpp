2476cc33c283df585c43781889496abbbffba8a5
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-09-28 12:20:13 +0530

    Time: 505 ms (80.98%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,10 +1,7 @@
-SELECT Day, ROUND( SUM(cancelled) / COUNT(cancelled), 2) as 'Cancellation Rate'
-FROM ( 
-    SELECT t.request_at as Day, t.status != 'completed' as cancelled 
-    FROM Trips t 
-    INNER JOIN Users u1 ON t.client_id = u1.users_id AND u1.banned = "No" 
-    INNER JOIN Users u2 ON t.driver_id = u2.users_id AND u2.banned = 'No'
-    WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
-) sub
-GROUP BY Day
-ORDER BY Day;
+SELECT t.request_at as Day, 
+    ROUND(SUM(t.status != 'completed')/COUNT(*), 2) as 'Cancellation Rate'
+FROM Trips t 
+INNER JOIN Users u1 ON t.client_id = u1.users_id AND u1.banned = "No" 
+INNER JOIN Users u2 ON t.driver_id = u2.users_id AND u2.banned = 'No'
+WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.request_at;
