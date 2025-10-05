d362e780f0eb27ccb13dd9e6a5090e07202a70ec
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-10-05 15:01:38 +0530

    Time: 32 ms (60.75%) | Memory: 60.4 MB (60.05%) - LeetSync

--- 630-course-schedule-iii/course-schedule-iii.cpp
+++ 630-course-schedule-iii/course-schedule-iii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
         int n = courses.size();
-        sort(courses.begin(), courses.end(), [&](auto a, auto b){
+        sort(courses.begin(), courses.end(), [&](auto& a, auto& b){
             return a[1] < b[1];
         });
         priority_queue<int> pq;
