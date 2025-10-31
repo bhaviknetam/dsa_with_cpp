5bb874207f0a370e0f9fc3fea2611b0e06eef563
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-10-31 23:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (68.37%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -7,14 +7,15 @@ public:
             if(ratings[i] > ratings[i - 1] && candies[i] <= candies[i-1]){
                 candies[i] = candies[i-1] + 1;
             }
-        }for(auto& val:candies)cout << val << ';';
+        }
+        // for(auto& val:candies)cout << val << ';';
         cout << endl;
         for(int i =  n - 2; i >= 0; i--){
             if(ratings[i] > ratings[i + 1] && candies[i] <= candies[i+1]){
                 candies[i] = max(candies[i], candies[i+1] + 1);
             }
         }
-        for(auto& val:candies)cout << val << ';';
+        // for(auto& val:candies)cout << val << ';';
         return accumulate(candies.begin(), candies.end(), 0);
     }
 };
