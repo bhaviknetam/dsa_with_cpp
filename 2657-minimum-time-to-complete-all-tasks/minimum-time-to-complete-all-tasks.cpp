54388d123f53a21ac2ecc485fbd3ae3f67c910d0
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-11-08 16:25:53 +0530

    Time: 14 ms (89.93%) | Memory: 44.2 MB (79.86%) - LeetSync

--- 2657-minimum-time-to-complete-all-tasks/minimum-time-to-complete-all-tasks.cpp
+++ 2657-minimum-time-to-complete-all-tasks/minimum-time-to-complete-all-tasks.cpp
@@ -1,3 +1,27 @@
+struct Fenwick{
+    vector<int> bit;
+    int n;
+    Fenwick(int _n) : n(_n+1){
+        bit.assign(n+1, 0);
+    }
+    void add(int idx, int delta){
+        for(;idx < n; idx += idx & -idx){
+            bit[idx] += delta;
+        }
+    }
+    int sum(int idx){
+        int res = 0;
+        for(;idx > 0; idx -= idx & -idx){
+            res += bit[idx];
+        }
+        return res;
+    }
+    int sum(int l, int r){
+        if(l > r) return 0;
+        return sum(r) - sum(l-1);
+    }
+};
+
 class Solution {
 public:
     int findMinimumTime(vector<vector<int>>& tasks) {
@@ -5,22 +29,21 @@ public:
             if(a[1] == b[1]) return a[0] < b[0];
             return a[1] < b[1];
         });
-        int n = tasks.size();
-        vector<bool> timeSlots(tasks[n-1][1] + 2, false);
+
+        int maxEnd = tasks.back()[1];
+        Fenwick fw(maxEnd + 2);
+
         for(auto& task : tasks){
             int start = task[0], end = task[1], duration = task[2];
-            for(int i = start; i <= end; i++){
-                if(timeSlots[i]) duration--;
-            }
-            int t = end;
-            while(duration > 0){
-                if(!timeSlots[t]){
-                    timeSlots[t] = true;
+            duration -= fw.sum(start, end);
+
+            for (int t = end; duration > 0; --t) {
+                if (fw.sum(t, t) == 0) {
+                    fw.add(t, 1);
                     duration--;
                 }
-                t--;
             }
         }
-        return accumulate(timeSlots.begin(), timeSlots.end(), 0);
+        return fw.sum(maxEnd);
     }
 };
