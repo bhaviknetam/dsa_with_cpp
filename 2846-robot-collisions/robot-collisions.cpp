b0dcb8c35916e9ca7aa26250bed245528648f1ce
Bhavik Netam <118985566+bhaviknetam@users.noreply.github.com>
2025-11-18 00:23:20 +0530

    Time: 81 ms (51.96%) | Memory: 222.1 MB (50.69%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -1,53 +1,46 @@
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        vector<int> ans;
-        int n = healths.size();
-        vector<pair<int,int>> temp;
-        map<int,pair<int,int>> m;
-        for(int i=0;i<n;i++){
-            healths[i] = (directions[i]=='R')?healths[i]:-healths[i];
-            m[positions[i]]={i, healths[i]};
+        vector<pair<int, pair<char, pair<int, int>>>> info;
+        int n = positions.size();
+        for(int i = 0; i < n; i++){
+            info.push_back({positions[i], {directions[i], {healths[i], i}}});
         }
-        stack<pair<int,int>> s;
-        for(auto& i : m){
-            int index = i.second.first;
-            int health = i.second.second;
-            if(health > 0){
-                s.push({index, health});
-            }else{
-                bool push = true;
-                while(!s.empty()){
-                    int top = s.top().second;
-                    int topidx = s.top().first;
-                    if(top > abs(health)){
-                        int newhealth = top - 1;
-                        s.pop();
-                        s.push({topidx, newhealth});
-                        break;
-                    }else if(top < abs(health)){
-                        health = (health<0)?health+1:health-1;
-                        s.pop();
-                        continue;
-                    }else{
-                        s.pop();
-                        if(s.empty()) push = false;
-                        break;
-                    }
-                }
-                if(push and s.empty()){
-                    temp.push_back({index, health});
+        sort(info.begin(), info.end());
+        cout << info.size() << endl;
+        stack<pair<char, pair<int, int>>> s;
+        for(auto& vals : info){
+            char dir = vals.second.first;
+            int currH = vals.second.second.first;
+            int idx = vals.second.second.second;
+            bool flag = false;
+            while(!s.empty() && s.top().first == 'R' && dir == 'L'){
+                int& prevH = s.top().second.first;
+                if(prevH > currH){
+                    prevH = prevH - 1;
+                    flag = true;
+                    break;
+                }else if(currH > prevH){
+                    s.pop();
+                    currH = currH - 1;
+                }else{
+                    s.pop();
+                    flag = true;
+                    break;
                 }
             }
+            if(!flag) s.push({dir, {currH, idx}});
         }
+        vector<int> ans(n, -1);
         while(!s.empty()){
-            temp.push_back(s.top());
+            auto& [dir, curr] = s.top();
+            ans[curr.second] = curr.first;
             s.pop();
         }
-        sort(temp.begin(), temp.end());
-        for(auto& i : temp){
-            ans.push_back(abs(i.second));
-        }
-        return ans;
+        vector<int> res;
+        for(int i = 0; i < n; i++)
+            if(ans[i] != -1) res.push_back(ans[i]);
+        
+        return res;
     }
 };
